9d6f33dcd06626f7345288c363e78a7bf68cf05f
ka-reem <kareemalmond@gmail.com>
2024-04-07 00:39:45 -0700

    test push

--- python_test.py
+++ python_test.py
@@ -1,10 +1,10 @@
 import requests
 
 # Replace with your ESP32's IP address and endpoint
-url = 'http://ESP32_IP_ADDRESS/data'
+url = 'http://172.20.10.12/data'
 
 # Replace with the integer value you want to send
-data = {'value': 123}
+data = {'value': 1}
 
 # Send a POST request with JSON payload
 response = requests.post(url, json=data)
