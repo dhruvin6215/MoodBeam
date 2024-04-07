25f1e6a076d3b3b0f67fe7138dee5ac0047aa753
Elliot Verduzco <elliot@elliotverduzco.com>
2024-04-07 01:51:32 -0700

    Update HackESP.ino
    working version!

--- HackESP.ino
+++ HackESP.ino
@@ -3,47 +3,44 @@
 #include <ArduinoJson.h>
 
 // Network Credentials
-const char* ssid = "Elliot iPhone";
+const char* ssid = "Elliot iPhone"; //temporary credentials
 const char* password = "SFhacks24";
-int value = 8;
+int value = 0;
 WebServer server(80);
 
-void setup() {
-  Serial.begin(115200); // Start the Serial communication
-  
-  
-  WiFi.begin(ssid, password); // Connect to the network
-
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    //Serial.println("Connecting to WiFi...");
+void handleData() {
+  if (server.hasArg("plain") == false) { // Check if body received
+    server.send(400, "text/plain", "Bad Request");
+    return;
   }
 
-  //Serial.println("Connected to WiFi");
-  Serial.print("W"); // Long Pulse to indicate WiFi connection
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, server.arg("plain"));
+  value = doc["value"]; // JSON data
+  Serial.println(value); // Print value to the serial 
+
+  server.send(200, "text/plain", "Data received");
+}
+
+void setup() {
+  Serial.begin(115200); 
+  WiFi.begin(ssid, password); // Connect to the network
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.print("Connected to WiFi, IP address: ");
   Serial.println(WiFi.localIP());
   
-  server.begin(); // Start the server
+  // Define route for the data
+  server.on("/data", HTTP_POST, handleData);
   
+  server.begin(); // Start the server
+  Serial.println("HTTP server started");
 }
 
 void loop() {
   server.handleClient(); // Handle client requests
-  Serial.print(value);    // Send the value to the Arduino
-  delay(5000);          // Wait for 5 seconds
-
-  // receiving from .py script
-
-  if (Serial.available() > 0) {
-    // Read the incoming number
-    int sfsu_score = Serial.parseInt();
-
-    // Print the received number
-    Serial.print("Received sfsu score: ");
-    Serial.println(sfsu_score);
-  }
-
-
-
 }
